5e78deee6e4ca11d99c896778e6a6f8f0dad849c
ricardoariasm762 <ricardo.ariasm@campusucc.edu.co>
2025-03-05 10:38:31 -0500

    creo carpetas y archivos de service y screem

---
+++ lib/services/location_service.dart
@@ -0,0 +1,25 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+
+  Future<Position> getCurrentLocation() async {
+
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      throw Exception("GPS desactivado");
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    return await Geolocator.getCurrentPosition();
+
+  }
+}
