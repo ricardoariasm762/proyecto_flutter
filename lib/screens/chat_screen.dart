4a222e932df6ae93457fa09ec34256eac9f6415d
cristiancg11 <cristian.gomezsanta@campusucc.edu.co>
2026-03-25 14:07:28 -0500

    agrego lista básica de mensajes en chat

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
+  final List<String> mensajes = [
+    "Hola!",
+    "Vas a un lugar cerca a la universidad?",
+    "Sí, vamos juntos"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +14,13 @@ class ChatScreen extends StatelessWidget {
         title: Text("Chat"),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Text("Aquí irá el chat"),
+      body: ListView.builder(
+        itemCount: mensajes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(mensajes[index]),
+          );
+        },
       ),
     );
   }
