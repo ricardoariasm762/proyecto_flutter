cc599baa8675f9a6eceabe3cffb22442e507d2f1
Nicolas Wagimin Bravo <nicolaswagiminbravo@Nicolass-MacBook-Air.local>
2026-04-01 14:51:05 -0500

    feat: implement data layer (datasources and repository implementations)

---
+++ lib/data/datasources/location_local_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationLocalDataSource {
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception("GPS desactivado");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
